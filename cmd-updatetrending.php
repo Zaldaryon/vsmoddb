2be609c4aba23a5451acf8698a99df855903e92e
Rennorb <18741506+SaculRennorb@users.noreply.github.com>
2026-06-27 16:36:29 +0200

    updated trendingpoints calculations

--- cmd-updatetrending.php
+++ cmd-updatetrending.php
@@ -13,23 +13,50 @@ if(DB_READONLY) {
 	exit();
 }
 
+$smoothingBias = 5;
+$intervalHours = 24;
+
 $ok = $con->execute(<<<SQL
 	UPDATE mods m
 	LEFT JOIN (
-		SELECT c.assetId, COUNT(c.commentId) AS comments
+		SELECT c.assetId, COUNT(*) AS comments
 		FROM comments c
-		WHERE c.created > DATE_SUB(NOW(), INTERVAL 72 HOUR)
+		WHERE c.created > DATE_SUB(NOW(), INTERVAL $intervalHours HOUR)
 		GROUP BY c.assetId
 	) c1 ON c1.assetId = m.assetId
+	LEFT JOIN (
+		SELECT c.assetId, COUNT(*) AS comments
+		FROM comments c
+		WHERE c.created BETWEEN DATE_SUB(NOW(), INTERVAL 2 * $intervalHours HOUR) AND DATE_SUB(NOW(), INTERVAL $intervalHours HOUR)
+		GROUP BY c.assetId
+	) c2 ON c2.assetId = m.assetId
 	LEFT JOIN (
 		SELECT r.modId, COUNT(*) as downloads
 		FROM fileDownloadTracking d
 		JOIN files f ON f.fileId = d.fileId
 		JOIN modReleases r ON r.assetId = f.assetId
-		WHERE d.lastDownload > DATE_SUB(NOW(), INTERVAL 72 HOUR)
+		WHERE d.lastDownload > DATE_SUB(NOW(), INTERVAL $intervalHours HOUR)
 		GROUP BY r.modId
 	) f1 ON f1.modId = m.modId
-	SET m.trendingPoints = IFNULL(f1.downloads, 0) + 5 * IFNULL(c1.comments, 0)
+	LEFT JOIN (
+		SELECT r.modId, COUNT(*) as downloads
+		FROM fileDownloadTracking d
+		JOIN files f ON f.fileId = d.fileId
+		JOIN modReleases r ON r.assetId = f.assetId
+		WHERE d.lastDownload BETWEEN DATE_SUB(NOW(), INTERVAL 2 * $intervalHours HOUR) AND DATE_SUB(NOW(), INTERVAL $intervalHours HOUR)
+		GROUP BY r.modId
+	) f2 ON f2.modId = m.modId
+	SET m.trendingPoints = ROUND
+  (
+    (
+        (COALESCE(f1.downloads, 0) * LEAST($intervalHours * 60 / LEAST(TIMESTAMPDIFF(MINUTE, m.created, NOW()), $intervalHours * 60), 10) + $smoothingBias)
+      / (COALESCE(f2.downloads, 0) * GREATEST(0, LEAST($intervalHours * 60 / LEAST(TIMESTAMPDIFF(MINUTE, m.created, NOW()) - $intervalHours * 60, $intervalHours * 60), 10)) + $smoothingBias)
+    ) + 
+    (
+        (COALESCE(c1.comments,  0) * LEAST($intervalHours * 60 / LEAST(TIMESTAMPDIFF(MINUTE, m.created, NOW()), $intervalHours * 60), 10) + $smoothingBias)
+      / (COALESCE(c2.comments, 0) * GREATEST(0, LEAST($intervalHours * 60 / LEAST(TIMESTAMPDIFF(MINUTE, m.created, NOW()) - $intervalHours * 60, $intervalHours * 60), 10)) + $smoothingBias)
+    ) * 5 - 6
+  )
 SQL);
 
 if(!$ok) http_response_code(HTTP_INTERNAL_ERROR);
